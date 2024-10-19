30e3f884751161f038fa6c0517fc63bd07beb2d2
sundossaifi <sundossaifi99@gmail.com>
2024-10-19 19:52:46 +0300

    Paginate the GET /notes

--- routes/notes.js
+++ routes/notes.js
@@ -4,11 +4,32 @@ const mongoose = require('mongoose');
 const Note = require('../models/note');
 
 router.get('/', async (req, res) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < await Note.countDocuments().exec()) {
+        results.next = {
+            page: page + 1,
+            limit: limit
+        }
+    }
+
+    if (startIndex > 0) {
+        results.previous = {
+            page: page - 1,
+            limit: limit
+        }
+    }
     try {
-        const notes = await Note.find()
-        res.status(200).json(notes);
+        results.notes = await Note.find().limit(limit).skip(startIndex).exec();
+        res.status(200).json({results});
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        res.status(500).json({ message: error.message });
     }
 });
 
